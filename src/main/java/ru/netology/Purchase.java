589e356b362c6c3f125d05c4bf470ba66866d691
amiranify <amiranify@mail.ru>
2023-01-16 11:46:29 +0300

    first commit

---
+++ src/main/java/ru/netology/Purchase.java
@@ -0,0 +1,11 @@
+package ru.netology;
+
+public class Purchase {
+    protected String title;
+    protected int count;
+
+    public Purchase(String title, int count) {
+        this.title = title;
+        this.count = count;
+    }
+}
